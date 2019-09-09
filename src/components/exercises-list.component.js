a0be86c7fe323a439e2718c1b689ebdf3029bbfd
Florian Dufter <florian.dufter@iteratec.com>
2019-09-09 09:07:36 +0200

    *added bootstrap to frontend. *Preparation for react components

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                You are on the exercises list component.
+                </p>
+            </div>
+        )
+    }
+}
